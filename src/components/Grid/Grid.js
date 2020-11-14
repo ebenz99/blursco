a300012bdfd398ca54b033227854194de021ea53
Ethan Bensman <ebenz99@gmail.com>
2020-11-14 14:35:42 -0500

    tiling works

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,28 +1,54 @@
 import './Grid.scss';
 import ProgressiveImage from "react-progressive-image";
+import shuffle from 'lodash/shuffle';
 
 function Grid() {
-    const myimg = "./imgs/lizard.jpeg"
+    const numJPGs = 62;
+    const numPNGs = 13;
+    const numImgs = numJPGs + numPNGs;
+    // const numImgs = 5;
+    let imgs = []
+    let extension = 'jpg'
+    for (let i=0; i < numImgs; i+=1){
+        if (i >= numJPGs) {
+            extension = 'png'
+        }
+        imgs.push(
+            <div className="pod">
+                <ProgressiveImage 
+                    src={process.env.PUBLIC_URL + `/photos/${i}.${extension}`} 
+                    placeholder={process.env.PUBLIC_URL + `/placeholders/${i}.${extension}`}>
+                    {(src, loading) => (
+                        <img style={{ filter: loading ? "blur(5px)" : "blur(0)" }} src={src} alt="projects" />
+                    )}
+                </ProgressiveImage>
+            </div>
+        )
+    }
+    let shuffledImgs = shuffle(imgs);
+    let third = Math.floor(shuffledImgs.length/3);
     return (
         <div className="grid">
             <div className="column">
-                <div className="pod">
-                    <ProgressiveImage 
-                        src={'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'} 
-                        placeholder={process.env.PUBLIC_URL + '/imgs/lizard.jpeg'}>
-                        {(src, loading) => (
-                            <img style={{ filter: loading ? "blur(5px)" : "blur(0)" }} src={src} alt="projects" />
-                        )}
-                    </ProgressiveImage>
-                </div>
+                {shuffledImgs.slice(0,third)}
+            </div>
+            <div className="column">
+                {shuffledImgs.slice(third,third*2)}
+            </div>
+            <div className="column">
+                {shuffledImgs.slice(third*2,shuffledImgs.length)}
             </div>
         </div>
 
     );
 }
 
-{/* <ProgressiveImage src="/imgs/lizard.jpeg" placeholder="/imgs/lizardcopysmall.jpeg">
-{src => <img src={src} alt="an image" />}
+{/* <ProgressiveImage 
+src={'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'} 
+placeholder={process.env.PUBLIC_URL + '/imgs/lizard.jpeg'}>
+{(src, loading) => (
+    <img style={{ filter: loading ? "blur(5px)" : "blur(0)" }} src={src} alt="projects" />
+)}
 </ProgressiveImage> */}
 
 export default Grid;

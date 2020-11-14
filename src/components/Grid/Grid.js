95340f0c2e348318179e9d4396dd5ed4619874ad
Ethan Bensman <ebenz99@gmail.com>
2020-11-13 21:34:44 -0500

    load into right size

---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,23 @@
+import './Grid.scss';
+import ProgressiveImage from "react-progressive-image";
+
+function Grid() {
+    const myimg = "./imgs/lizard.jpeg"
+    return (
+        <div className="grid">
+            <ProgressiveImage 
+                src={'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'} 
+                placeholder={process.env.PUBLIC_URL + '/imgs/lizard.jpeg'}>
+                {(src, loading) => (
+                    <img style={{ filter: loading ? "blur(5px)" : "blur(0)" }} src={src} alt="projects" />
+                )}
+            </ProgressiveImage>
+        </div>
+    );
+}
+
+{/* <ProgressiveImage src="/imgs/lizard.jpeg" placeholder="/imgs/lizardcopysmall.jpeg">
+{src => <img src={src} alt="an image" />}
+</ProgressiveImage> */}
+
+export default Grid;
